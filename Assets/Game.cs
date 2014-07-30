5b54892d11363ae15117275b118c8fef13a5b56f
Louda, Michal <mloud@seznam.cz>
2014-07-30 08:57:48 +0200

    IntitialDrop

---
+++ Assets/Game.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Game : MonoBehaviour
+{
+	[SerializeField]
+	List<Segment> Segments;
+
+	
+	void Update () 
+	{
+		for (int i = 0; i < Segments.Count; ++i)
+		{
+			for (int j = i + 1; j < Segments.Count; ++j)
+			{
+
+
+			}
+		}
+	}
+
+
+	bool SegmetConnected(Segment seg1, Segment seg2)
+	{
+
+		for (int i = 0; i < seg1.Connectors.Count; ++i)
+		{
+			for (int j = 0; j < seg2.Connectors.Count; ++j)
+			{
+				//project points to screenspace - if in touch, they are connected
+				
+			}
+
+		}
+	}
+
+}
---
+++ Assets/Segment.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Segment : MonoBehaviour
+{
+	[SerializeField]
+	public List<Transform> Connectors;
+
+
+	void Update ()
+	{
+	
+	}
+}
