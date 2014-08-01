ee99e33a1208323ee73e1452bc113dd65c12ff69
Louda, Michal <mloud@seznam.cz>
2014-08-01 14:58:11 +0200

    Update

--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -11,24 +11,24 @@ public class SceneController : UI.ITouchListener
 	}
 	
 	public Transform RootPoint { get; set; }
-
-
+	
+	
 	private Vector3 _initialRotation;
-	private UI.Touch _touchBegan;
+	private UI.Touch? _touchBegan;
 	private Zone _touchZone;
-
-
+	
+	
 	public SceneController()
 	{
 		UI.TouchManager.Instance.Register(this);
 	}
-
+	
 	public void Release()
 	{
 		UI.TouchManager.Instance.Unregister(this);
 	}
-
-
+	
+	
 	private Zone ResolveTouchZone(Vector3 pos)
 	{
 		Zone zone = Zone.None;
@@ -45,30 +45,33 @@ public class SceneController : UI.ITouchListener
 		return zone;
 		
 	}
-
+	
 	public void TouchBegan(UI.Touch touch)
 	{
-		_initialRotation = RootPoint.transform.localRotation.eulerAngles;
-
-		_touchBegan = touch;
-	
-		_touchZone = ResolveTouchZone(touch.Position);
+		if (_touchBegan == null)
+		{
+			_initialRotation = RootPoint.transform.localRotation.eulerAngles;
+			
+			_touchBegan = touch;
+			
+			_touchZone = ResolveTouchZone(touch.Position);
+		}
 	}
 	
 	
 	public void TouchEnded(UI.Touch touch)
 	{
-
+		_touchBegan = null;
 	}
 	
 	public void TouchMoved(UI.Touch touch)
 	{
 		if (_touchZone != Zone.None)
 		{
-			Vector3 touchBeginEndVec = touch.Position - _touchBegan.Position;
-	
+			Vector3 touchBeginEndVec = touch.Position - _touchBegan.Value.Position;
+			
 			Vector3 rotation = new Vector3();
-
+			
 			if (_touchZone == Zone.Bottom)
 			{
 				rotation.y = touchBeginEndVec.x * 0.2f;
@@ -77,9 +80,9 @@ public class SceneController : UI.ITouchListener
 			{
 				rotation.x = touchBeginEndVec.y * 0.2f;
 			}
-
+			
 			RootPoint.transform.eulerAngles = _initialRotation + rotation;
 		}
 	}
-
+	
 }
