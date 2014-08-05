58da8ef098b615cf587550a89664a0ad0af065d6
Louda, Michal <mloud@seznam.cz>
2014-08-05 10:52:02 +0200

    Update gizmos

---
+++ Assets/Box.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Box : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.yellow;
+
+		float length = 8;
+
+		Vector3 direction = transform.TransformDirection(Vector3.right);
+
+		Vector3 p1 = transform.position - direction * length;
+		Vector3 p2 = transform.position + direction * length;
+
+		Gizmos.DrawLine(p1, p2);
+	}
+}
